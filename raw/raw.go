033aebac43585135a52af5b3d69543f207f5ea13
xitongsys <xitongsys@gmail.com>
2019-07-15 10:23:10 -0400

    linklayer

--- raw/raw.go
+++ raw/raw.go
@@ -2,8 +2,8 @@ package raw
 
 import (
 	"syscall"
+	"net"
 
-	"github.com/xitongsys/ptcp/header"
 	"github.com/xitongsys/ptcp/util"
 	"github.com/mdlayher/ethernet"
 )
@@ -12,61 +12,64 @@ var RAWBUFSIZE = 65535
 
 type Raw struct {
 	ifName string
-	fdWrite int
-	fdRead int
+	iface *net.Interface
+	fd int
 	buf []byte
 }
 
 func NewRaw(interfaceName string) (*Raw, error){
-	fdW, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_RAW)
+	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(util.Htons(syscall.ETH_P_ALL)))
 	if err != nil {
 		return nil, err
 	}
-
-	fdR, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(util.Htons(syscall.ETH_P_ALL)))
-	if err != nil {
-		return nil, err
-	}
-
-	/*
+	
 	iface, err := net.InterfaceByName(interfaceName)
 	if err != nil {
 		return nil, err
 	}
-	*/
 
-	if err = syscall.BindToDevice(fdR, interfaceName); err!=nil{
+	if err = syscall.BindToDevice(fd, interfaceName); err!=nil{
 		return nil, err
 	}
-	if err = syscall.SetsockoptInt(fdR, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err!=nil{
+	if err = syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err!=nil{
 		return nil, err
 	}
 
 	return &Raw{
 		ifName: interfaceName,
-		fdWrite: fdW, 
-		fdRead: fdR,
+		iface: iface,
+		fd: fd,
 		buf: make([]byte, RAWBUFSIZE),
 	}, nil
 }
 
 func (r *Raw) Read() ([]byte, error) {
-	n, _, err := syscall.Recvfrom(r.fdRead, r.buf, 0)
+	n, _, err := syscall.Recvfrom(r.fd, r.buf, 0)
 	
 	if err == nil {
 		eth := &ethernet.Frame{}
-		eth.UnmarshalBinary(r.buf[:n])
+		err = eth.UnmarshalBinary(r.buf[:n])
 		return eth.Payload, err
 	}
 	return nil, err
 }
 
 func (r *Raw) Write(data []byte, addrs string) error {
-	ip, port := header.ParseAddr(addrs)
-
-	addr := syscall.SockaddrInet4 {
-		Port: port,
-		Addr: header.IpStr2Bytes(ip),
+	eth := &ethernet.Frame{}
+	eth.EtherType = ethernet.EtherTypeIPv4
+	eth.Destination = ethernet.Broadcast
+	eth.Source = r.iface.HardwareAddr
+	eth.Payload = data
+	data, err := eth.MarshalBinary()
+	if err != nil {
+		return err
 	}
-	return syscall.Sendto(r.fdWrite, data, 0, &addr)
+
+	addr := syscall.SockaddrLinklayer{
+		Halen: 6,
+		Addr: [8]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+		Ifindex: r.iface.Index,
+	}
+
+	return syscall.Sendto(r.fd, data, 0, &addr)
 }
