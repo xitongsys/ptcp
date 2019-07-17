966311a627c640b82df04fb3858a652d6490dfa2
xitongsys <xitongsys@gmail.com>
2019-07-17 11:36:02 +0800

    update ip

--- example/server.go
+++ example/server.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	ptcp.Init("eth0", 2)
-	ln, err := ptcp.Listen("ptcp", "172.17.41.89:12222")
+	ln, err := ptcp.Listen("ptcp", "127.0.0.1:12222")
 	if err != nil {
 		fmt.Println(err)
 		return
